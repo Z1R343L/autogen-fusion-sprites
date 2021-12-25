ed34e5c00f48364660a8e5347ba462cfddeb643e
Z1R343L <jan2705g@gmail.com>
2021-12-25 08:03:26 +0000

    update trim.py

--- script/trim.py
+++ script/trim.py
@@ -1,19 +1,39 @@
+GNU nano 5.4                                      util.py
 import os
 from glob import glob
 
+import typer
 from PIL import Image
+import click_spinner
+import alive_progress
 
-trim_path = "../Battlers_trim/"
-if not os.path.exists(trim_path):
-    os.mkdir(trim_path)
+src_path = '../Battlers/'
+trim_path = '../Battlers_trim/'
 
-for dir in glob("../Battlers/*"):
-    for fp in glob(f"{dir}/*.png"):
-        fn = fp.split("/")[-1]
-        dest_path = f"{trim_path}{fn}"
-        if not os.path.exists(dest_path):
-            im = Image.open(fp).convert("RGBA")
-            print(im.size)
-            im_trim = im.crop(im.getbbox())
-            im_trim.save(dest_path, "PNG")
-            print(fn)
+app = typer.Typer()
+
+def list_files() -> list:
+    r = []
+    for dir in glob(f"{src_path}*"):
+       for fp in glob(f"{dir}/*.png"):
+           r.append(fp)
+    return r
+
+@app.command()
+def trim(quant: bool = False, force: bool = False) -> None:
+    """trims away unused space of the sprites"""
+    with click_spinner.spinner():
+        pathlist = list_files()
+
+    with alive_progress.alive_bar(len(pathlist)) as bar:
+        for fp in pathlist:
+            fn = fp.split("/")[-1]
+            dest_path = f"{trim_path}{fn}"
+            if not os.path.exists(dest_path) or force:
+                im = Image.open(fp)
+                im_trim = im.crop(im.getbbox())
+                im_trim.save(dest_path, "PNG")
+            bar()
+
+if __name__ == "__main__":
+    app()
